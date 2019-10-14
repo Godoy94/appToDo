e9fcb73cc4923259aa025cff65108bbe993a2a0b
Godoy94 <domingues.godoy@hotmail.com.br>
2019-10-14 15:13:46 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoList';
+
+  public tarefa = "";
+  public items = [];
+
+  addTarefa(){
+    this.items.push(this.tarefa);
+  }
+
+  removerTarefa(item){
+    this.items.splice(this.items.indexOf(item), 1);
+  }
+
 }
